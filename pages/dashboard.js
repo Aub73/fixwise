d9c802f22051947490322f3561209c5d689ca853
Kartikey Jaiswal <63246079+karan74jaiswal@users.noreply.github.com>
2025-07-23 03:07:28 +0530

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,12 +1,1 @@
-import Navbar from "../components/Navbar";
-export default function Dashboard() {
-  return (
-    <div>
-      <Navbar />
-      <main style={{ padding: "2rem" }}>
-        <h1>Dashboard Page</h1>
-        <p>This is the dashboard page of Fixwise.</p>
-      </main>
-    </div>
-  );
-}
+
