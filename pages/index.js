db610755a9fdbeed8e521be8cb9c612b101d6bb8
Kartikey Jaiswal <63246079+karan74jaiswal@users.noreply.github.com>
2025-07-23 03:07:43 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,1 @@
-import Navbar from "../components/Navbar";
-export default function Index() {
-  return (
-    <div>
-      <Navbar />
-      <main style={{ padding: "2rem" }}>
-        <h1>Index Page</h1>
-        <p>This is the index page of Fixwise.</p>
-      </main>
-    </div>
-  );
-}
+
