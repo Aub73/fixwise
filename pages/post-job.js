edaf9e1662156688c712e851ab800a7b62578fa0
Kartikey Jaiswal <63246079+karan74jaiswal@users.noreply.github.com>
2025-07-23 03:08:47 +0530

    Update post-job.js

--- pages/post-job.js
+++ pages/post-job.js
@@ -1,12 +1,1 @@
-import Navbar from "../components/Navbar";
-export default function PostJob() {
-  return (
-    <div>
-      <Navbar />
-      <main style={{ padding: "2rem" }}>
-        <h1>Post Job Page</h1>
-        <p>This is the post job page of Fixwise.</p>
-      </main>
-    </div>
-  );
-}
+
