6c7e434732df51f6dd86bd406ae066741ba88aab
Kartikey Jaiswal <63246079+karan74jaiswal@users.noreply.github.com>
2025-07-23 03:08:30 +0530

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,1 @@
-import Navbar from "../components/Navbar";
-export default function Login() {
-  return (
-    <div>
-      <Navbar />
-      <main style={{ padding: "2rem" }}>
-        <h1>Login Page</h1>
-        <p>This is the login page of Fixwise.</p>
-      </main>
-    </div>
-  );
-}
+
