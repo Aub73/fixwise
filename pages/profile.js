13f38a6a2dad107d89da3446cd3b203d5aee82fe
Kartikey Jaiswal <63246079+karan74jaiswal@users.noreply.github.com>
2025-07-23 03:09:08 +0530

    Update profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -1,12 +1,1 @@
-import Navbar from "../components/Navbar";
-export default function Profile() {
-  return (
-    <div>
-      <Navbar />
-      <main style={{ padding: "2rem" }}>
-        <h1>Profile Page</h1>
-        <p>This is the profile page of Fixwise.</p>
-      </main>
-    </div>
-  );
-}
+
