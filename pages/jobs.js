5f15a9fadda107eabb390575c6f48a5f9a41ce29
Kartikey Jaiswal <63246079+karan74jaiswal@users.noreply.github.com>
2025-07-23 03:07:57 +0530

    Update jobs.js

--- pages/jobs.js
+++ pages/jobs.js
@@ -1,12 +1,1 @@
-import Navbar from "../components/Navbar";
-export default function Jobs() {
-  return (
-    <div>
-      <Navbar />
-      <main style={{ padding: "2rem" }}>
-        <h1>Jobs Page</h1>
-        <p>This is the jobs page of Fixwise.</p>
-      </main>
-    </div>
-  );
-}
+
